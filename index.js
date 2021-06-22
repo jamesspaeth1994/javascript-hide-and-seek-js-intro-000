2cc1d6dce41b0b5b8ba4611d7df04db102155bb2
reflective-foobar-0518 <jamesspaeth1994@gmail.com>
2021-06-22 01:39:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list');
-  for (let i = 0; i < upRank.length; i++) {
-    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n);
+  for (let i = 0; i < ranks.length; i++) {
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
   }
 }
 
